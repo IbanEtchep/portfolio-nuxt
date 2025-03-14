8eddd805c2e80f7157bc5d6e3c50b7347a04bc10
IbanEtchep <49568721+IbanEtchep@users.noreply.github.com>
2025-03-14 16:34:11 +0000

    Deploying to gh-pages from @ IbanEtchep/portfolio-nuxt@ad7f95eac5e57df53e0897996f13eedc6d28c47c 🚀

---
+++ _nuxt/Xx-TJyib.js
@@ -0,0 +1,1 @@
+import{j as r,c as o,i as t,Y as n}from"./C7ncT8Ch.js";const s={};function c(e,a){return t(),o("strong",null,[n(e.$slots,"default")])}const f=r(s,[["render",c]]);export{f as default};
