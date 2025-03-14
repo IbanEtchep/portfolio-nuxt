701abd5ffb56dd42b928f06e289bf198c759bbc6
IbanEtchep <49568721+IbanEtchep@users.noreply.github.com>
2025-03-14 16:26:16 +0000

    Deploying to gh-pages from @ IbanEtchep/portfolio-nuxt@41cc254cc9a7e8a1ad8cd8dbf77b1ce92c6f52c6 🚀

--- i18n/locales/eu.js
+++
@@ -1,4 +0,0 @@
-export default {
-    welcome_1: 'Ongi etorri, <span class="colored">Iban</span> deitzen naiz.',
-    welcome_2: '<span class="colored">Garatzaile</span> sutsua, ideiak egi bilakaraztea gustatzen zait.',
-}
